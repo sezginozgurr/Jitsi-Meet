a523e320e72da4b608e115598e9dcee2add62ab3
sezginozgurr <sezginzgurr@gmail.com>
2021-03-11 16:01:49 +0300

    Whats Problem?

--- app/src/main/java/com/example/jeetsemeet/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/jeetsemeet/activities/SignUpActivity.kt
@@ -10,7 +10,7 @@ import com.example.jeetsemeet.util.Constant
 import com.example.jeetsemeet.util.LocalDataManager
 import com.google.firebase.firestore.FirebaseFirestore
 
-class   SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySignUpBinding
 
@@ -19,8 +19,8 @@ class   SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnSignUp.setOnClickListener {
-            if (isValid()){
-
+            if (isValid()) {
+                signUp()
             }
         }
     }
@@ -36,38 +36,58 @@ class   SignUpActivity : AppCompatActivity() {
             Toast.makeText(this, "Lütfen boşlukları doldurun.", Toast.LENGTH_SHORT).show()
             return false
         }
-        signUp()
         return true
     }
 
-    fun signUp(){
-        binding.btnSignUp.visibility = View.INVISIBLE
+    fun signUp() {
+        binding.btnSignUp.visibility = View.VISIBLE
         binding.progressBar.visibility = View.VISIBLE
 
         val database = FirebaseFirestore.getInstance()
-        val user = HashMap<String, Any>()
-        user[Constant.KEY_FIRST_NAME] = binding.firstName.text.toString()
-        user[Constant.KEY_LAST_NAME] = binding.lastName.text.toString()
-        user[Constant.KEY_EMAIL] = binding.email.text.toString()
-        user[Constant.KEY_PASSWORD] = binding.password.text.toString()
+        val users = HashMap<String, Any>()
+        users[Constant.KEY_FIRST_NAME] = binding.firstName.text.toString()
+        users[Constant.KEY_LAST_NAME] = binding.lastName.text.toString()
+        users[Constant.KEY_EMAIL] = binding.email.text.toString()
+        users[Constant.KEY_PASSWORD] = binding.password.text.toString()
 
 
 
-        database.collection(Constant.KEY_COLLECTION_USERS).add(user)
-            .addOnSuccessListener {
-                Toast.makeText(this,"User İnserted",Toast.LENGTH_SHORT).show()
-                LocalDataManager.instance.putString(this@SignUpActivity, Constant.KEY_FIRST_NAME, binding.firstName.text.trim().toString())
-                LocalDataManager.instance.putString(this@SignUpActivity, Constant.KEY_LAST_NAME, binding.lastName.text.trim().toString())
-                LocalDataManager.instance.putString(this@SignUpActivity, Constant.KEY_EMAIL, binding.email.text.trim().toString())
-                LocalDataManager.instance.putString(this@SignUpActivity, Constant.KEY_PASSWORD, binding.password.text.trim().toString())
-                LocalDataManager.instance.putBoolean(this@SignUpActivity, Constant.KEY_IS_SIGNED_IN,true)
-                startActivity(Intent(this,MainActivity::class.java))
+        database.collection(Constant.KEY_COLLECTION_USERS).add(users)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(this, "User İnserted", Toast.LENGTH_SHORT).show()
+                LocalDataManager.instance.putString(
+                    this@SignUpActivity,
+                    Constant.KEY_FIRST_NAME,
+                    binding.firstName.text.trim().toString()
+                )
+                LocalDataManager.instance.putString(
+                    this@SignUpActivity,
+                    Constant.KEY_LAST_NAME,
+                    binding.lastName.text.trim().toString()
+                )
+                LocalDataManager.instance.putString(this@SignUpActivity, Constant.KEY_USER_ID, documentReference.id )
+                LocalDataManager.instance.putString(
+                    this@SignUpActivity,
+                    Constant.KEY_EMAIL,
+                    binding.email.text.trim().toString()
+                )
+                LocalDataManager.instance.putString(
+                    this@SignUpActivity,
+                    Constant.KEY_PASSWORD,
+                    binding.password.text.trim().toString()
+                )
+                LocalDataManager.instance.putBoolean(
+                    this@SignUpActivity,
+                    Constant.KEY_IS_SIGNED_IN,
+                    true
+                )
+                startActivity(Intent(this, MainActivity::class.java))
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
             }.addOnFailureListener {
                 binding.btnSignUp.visibility = View.VISIBLE
                 binding.progressBar.visibility = View.INVISIBLE
-                Toast.makeText(this,"Error: Zıbab Ivj",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Error: Zıbab Ivj", Toast.LENGTH_SHORT).show()
             }
     }
 }
